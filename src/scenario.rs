vhiribarren/wgpu-lite-wrapper

wip - working rework of camera to pass eye to shader

src/scenario.rs
@@ -97,10 +97,11 @@ impl WinitEventLoopHandler for ScenarioScheduler {
 
     fn on_update(&mut self, update_context: &UpdateContext) {
         self.scenario.camera_mut().update();
-        let camera_matrix = self.scenario.camera().get_camera_matrix();
-        self.scenario
-            .scene_mut()
-            .update(update_context, camera_matrix);
+        // TODO Should I keep the clone, or regorganize the code?
+        let scenario = &*self.scenario;
+        let camera = scenario.camera().camera.clone();
+        let scene = self.scenario.scene_mut();
+        scene.update(update_context, &camera);
         self.scenario.on_update(update_context);
     }
 

src/scene.rs
@@ -24,9 +24,10 @@ SOFTWARE.
 
 use std::{cell::RefCell, rc::Rc};
 
-use cgmath::{Matrix4, SquareMatrix};
+use cgmath::{SquareMatrix, Zero};
 
 use crate::{
+    cameras::Camera,
     draw_context::{DrawContext, Drawable, Uniform},
     scenario::UpdateContext,
 };
@@ -45,8 +46,14 @@ pub trait Scene {
 
 #[allow(clippy::manual_non_exhaustive)]
 pub struct Scene3DUniforms {
-    pub camera_uniform: Uniform<[[f32; 4]; 4]>,
+    pub camera_mat: Uniform<[[f32; 4]; 4]>,
+    pub camera_pos: Uniform<[f32; 3]>,
     _private: (),
+}
+
+pub struct CameraData {
+    pub matrix: cgmath::Matrix4<f32>,
+    pub position: cgmath::Point3<f32>,
 }
 
 pub struct Scene3D {
@@ -59,7 +66,8 @@ impl Scene3D {
         Scene3D {
             drawables: Vec::new(),
             scene_uniforms: Scene3DUniforms {
-                camera_uniform: Uniform::new(context, cgmath::Matrix4::identity().into()),
+                camera_mat: Uniform::new(context, cgmath::Matrix4::identity().into()),
+                camera_pos: Uniform::new(context, cgmath::Vector3::zero().into()),
                 _private: (),
             },
         }
@@ -67,10 +75,22 @@ impl Scene3D {
     pub fn scene_uniforms(&self) -> &Scene3DUniforms {
         &self.scene_uniforms
     }
-    pub fn update(&mut self, _context: &UpdateContext, camera_matrix: Matrix4<f32>) {
+    pub fn update(&mut self, _context: &UpdateContext, camera: &Camera) {
         self.scene_uniforms
-            .camera_uniform
-            .write_uniform(camera_matrix.into());
+            .camera_mat
+            .write_uniform(camera.get_camera_matrix().into());
+        self.scene_uniforms
+            .camera_pos
+            .write_uniform(camera.get_eye_position().into());
+    }
+
+    pub fn update_with_camera_data(&mut self, _context: &UpdateContext, camera_data: CameraData) {
+        self.scene_uniforms
+            .camera_mat
+            .write_uniform(camera_data.matrix.into());
+        self.scene_uniforms
+            .camera_pos
+            .write_uniform(camera_data.position.into());
     }
 }
 

vhiribarren/wgpu-igniter

src/scenario.rs
@@ -134,11 +134,11 @@ impl WinitEventLoopHandler for ScenarioScheduler {
 
     fn on_render(&mut self, render_context: &RenderContext, render_pass: wgpu::RenderPass<'_>) {
         self.scenario.camera_mut().update();
-        let camera_matrix = self.scenario.camera().get_camera_matrix();
-        self.scenario
-            .scene_mut()
-            .update(render_context, camera_matrix);
-        self.scenario.on_update(render_context);
+        // TODO Should I keep the clone, or regorganize the code?
+        let scenario = &*self.scenario;
+        let camera = scenario.camera().camera.clone();
+        let scene = self.scenario.scene_mut();
+        scene.update(render_context, &camera);
         let mut rpass = render_pass.forget_lifetime();
         let scenario = self.scenario.as_mut();
         scenario.scene().render(&mut rpass);

src/scene.rs
@@ -24,9 +24,10 @@ SOFTWARE.
 
 use std::{cell::RefCell, rc::Rc};
 
-use cgmath::{Matrix4, SquareMatrix};
+use cgmath::{SquareMatrix, Zero};
 
 use crate::{
+    cameras::Camera,
     draw_context::{DrawContext, Drawable, Uniform},
     scenario::RenderContext,
 };
@@ -45,8 +46,14 @@ pub trait Scene {
 
 #[allow(clippy::manual_non_exhaustive)]
 pub struct Scene3DUniforms {
-    pub camera_uniform: Uniform<[[f32; 4]; 4]>,
+    pub camera_mat: Uniform<[[f32; 4]; 4]>,
+    pub camera_pos: Uniform<[f32; 3]>,
     _private: (),
+}
+
+pub struct CameraData {
+    pub matrix: cgmath::Matrix4<f32>,
+    pub position: cgmath::Point3<f32>,
 }
 
 pub struct Scene3D {
@@ -59,7 +66,8 @@ impl Scene3D {
         Scene3D {
             drawables: Vec::new(),
             scene_uniforms: Scene3DUniforms {
-                camera_uniform: Uniform::new(context, cgmath::Matrix4::identity().into()),
+                camera_mat: Uniform::new(context, cgmath::Matrix4::identity().into()),
+                camera_pos: Uniform::new(context, cgmath::Vector3::zero().into()),
                 _private: (),
             },
         }
@@ -67,10 +75,23 @@ impl Scene3D {
     pub fn scene_uniforms(&self) -> &Scene3DUniforms {
         &self.scene_uniforms
     }
-    pub fn update(&mut self, _context: &RenderContext, camera_matrix: Matrix4<f32>) {
+
+    pub fn update(&mut self, _context: &RenderContext, camera: &Camera) {
         self.scene_uniforms
-            .camera_uniform
-            .write_uniform(camera_matrix.into());
+            .camera_mat
+            .write_uniform(camera.get_camera_matrix().into());
+        self.scene_uniforms
+            .camera_pos
+            .write_uniform(camera.get_eye_position().into());
+    }
+
+    pub fn update_with_camera_data(&mut self, _context: &RenderContext, camera_data: CameraData) {
+        self.scene_uniforms
+            .camera_mat
+            .write_uniform(camera_data.matrix.into());
+        self.scene_uniforms
+            .camera_pos
+            .write_uniform(camera_data.position.into());
     }
 }
 